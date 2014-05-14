0ae9a6676ee1411b17344519ceb19481adaee29f
KingBowser <king@bowser.pw>
2014-05-14 15:19:21 -0500

    added config to test header

--- test/_test_header.rb
+++ test/_test_header.rb
@@ -2,3 +2,11 @@ $LOAD_PATH << "#{Dir.pwd}/lib" # Dirty hack
 
 require 'dhcp'
 require 'test/unit'
+
+module TestConfig
+  SETTINGS    = YAML.load_file('test/test_config.yaml')
+
+  SERVER      = SETTINGS['dhcp']['server']
+  CONF_FILE   = SETTINGS['dhcp']['config']
+  LEASE_FILE  = SETTINGS['dhcp']['lease']
+end
