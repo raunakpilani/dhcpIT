a1ac1c5bcd5e5bf3efd93a94add16310a95eb9e4
KingBowser <king@bowser.pw>
2014-05-14 15:27:03 -0500

    fixed test config and added subnet option

--- test/_test_header.rb
+++ test/_test_header.rb
@@ -2,11 +2,14 @@ $LOAD_PATH << "#{Dir.pwd}/lib" # Dirty hack
 
 require 'dhcp'
 require 'test/unit'
+require 'yaml'
 
 module TestConfig
-  SETTINGS    = YAML.load_file('test/test_config.yaml')
+  SETTINGS    = ::YAML.load_file('test/test_config.yml')
 
-  SERVER      = SETTINGS['dhcp']['server']
-  CONF_FILE   = SETTINGS['dhcp']['config']
-  LEASE_FILE  = SETTINGS['dhcp']['lease']
+  SERVER      = SETTINGS['server']['address']
+  CONF_FILE   = SETTINGS['server']['config']
+  LEASE_FILE  = SETTINGS['server']['leases']
+
+  SUBNET      = SETTINGS['network']['subnet']
 end
